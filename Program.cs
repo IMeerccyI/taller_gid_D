3bcc0a896be59d4ea148ae9644b245d91639a22b
IMeerccyI <GCN2018282@est.univalle.edu>
2023-05-10 16:01:44 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace xddd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int result = suma(2, 3);
+            
+        }
+        public static int suma(int n1, int n2)
+        {
+            return n1+ n2;
+        }
+    }
+}
