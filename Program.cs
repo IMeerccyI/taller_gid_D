7d577696047d4d00fec615f54331643a52b90381
IMeerccyI <GCN2018282@est.univalle.edu>
2023-05-10 16:07:45 -0400

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -7,11 +7,17 @@
             Console.WriteLine("Hello, World!");
 
             int result = suma(2, 3);
+
+            string nombrecompleto = nombre("Nelson", "Garcia");
             
         }
         public static int suma(int n1, int n2)
         {
             return n1+ n2;
+        }
+        public static string nombre(string name, string lastname)
+        {
+            return name + lastname;
         }
     }
 }
